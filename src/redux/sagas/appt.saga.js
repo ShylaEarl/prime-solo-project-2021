46fee40c1f5a68bc2795fabd50fe230f9f54c14e
Shyla Earl <shyla.earl@gmail.com>
2021-05-30 15:18:16 -0500

    client profile conditional rendering functional

--- src/redux/sagas/appt.saga.js
+++ src/redux/sagas/appt.saga.js
@@ -4,15 +4,20 @@ import { put, takeEvery } from 'redux-saga/effects'; //takeLatest
 // worker Saga: will be fired on "FETCH_APPT" actions from client table page
 function* fetchAppt() {
   try {
-    const appt = yield axios.get(`/api/info`); // /${id} double check route!
+    const appt = yield axios.get(`/api/info/AddAppt`); // /${id} double check route!
     yield put({ type: 'SET_APPT', payload: appt.data });
   } catch (error) {
     console.log('Appt get request failed', error);
   }
 }
 
+// function addAppt(){
+  // post for adding appointment
+// }
+
 function* apptSaga() {
   yield takeEvery('FETCH_APPT', fetchAppt);
+  //yield takeEvery('ADD_APPT', addAppt);
 }
 
 export default apptSaga;
