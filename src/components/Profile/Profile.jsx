9c348379e7452cae510c23842242937eac2bf6f0
Shyla Earl <shyla.earl@gmail.com>
2021-05-30 12:46:05 -0500

    added input fields for client PUT route and conditioanl rendering

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 function Profile(props){ //props here to get specific client id? params? redux?
+
+    //you will need to create and set local state here for input updates
+    //sets local state for put request
+    const [full_name, setFullName] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip_code, setZipCode] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
 
     //functionality to route to a page
     const history = useHistory();
@@ -26,33 +36,43 @@ function Profile(props){ //props here to get specific client id? params? redux?
         dispatch({ type: 'FETCH_APPT' }); 
     }, []);
 
+    let updateClicked = false;
+
     //PUT route to update client information
-    const updateClientInfo = (event, client) => {
-        console.log('update', client);
-        //axios delete call to server on '/info' route
-        axios.put(`/api/info/${id}`)
-        .then((response) => {
-        console.log('put request', response);
-        //get call replaced by useEffect
-        //dispatch({ type: 'FETCH_CLIENT' }); 
-        })
-        .catch((error) => {
-        console.log('error in UPDATE', error);
-        });
+    const updateClientInfo = () => { //event, client
+        
+        // //axios put call to server on '/info' route 
+        // axios.put(`/api/info/${id}`)
+        // .then((response) => {
+        // console.log('put request', response);
+        // //get call replaced by useEffect
+        // //dispatch({ type: 'UPDATE_CLIENT_INFO', payload: ${id}??? }); do I include a payload here to get one specific client's details? 
+        // })
+        // .catch((error) => {
+        // console.log('error in UPDATE', error);
+        // });
+
+        updateClicked = false;
+        console.log('update', updateClicked);
+
     }
 
-    const renderUpdateInputs = () => {
-
+    const renderToInputs = () => {
+        updateClicked = true;
+        console.log('update', updateClicked);
     }
 
     //temporary functionality to access ApptDetails page
     const apptDetails = () => {
-        console.log('date appt name p tag clicked!');
+        console.log('appt details clicked!', );
         history.push('/ApptDetails');
     }
 
     return(
         <div className="card-whole">
+              {/* if(updateClicked == false show client info) 
+                from edan's wk3 challenge feedback...
+                <div className=${updateClicked ? 'card-half-left-inputs' : 'card-half-left'} */}
             <div className="card-half-left">
                 {/* how do I render the specific client's info? props? redux?*/}
                 <h2>Willow Rosa Lee </h2>
@@ -71,9 +91,52 @@ function Profile(props){ //props here to get specific client id? params? redux?
                 {/* <p>{props.client.email}</p> */}
 
                 {/* onClick renders to editable input feilds for client info and submit button, on click of submit renders back to updated info view */}
-                <button className="a2o-btn" onClick={renderUpdateInputs}>Update Info</button>
+                <button className="a2o-btn" onClick={renderToInputs}>Update Info</button>
             </div>
-
+            :
+                {/* : else (show editable input feilds with a submit button that 
+                    dispatches the action type UPDATE_CLIENT_INFO with a new payload 
+                    of the updated information from input feilds, on click of submit button, show client info with updates) */}
+            <div className="card-half-left-inputs"> 
+                <input type="text"
+                placeholder={client.full_name}
+                value={full_name}
+                onChange={(event) => setFullName(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.address}
+                value={address}
+                onChange={(event) => setAddress(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.city}
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.state}
+                value={state}
+                onChange={(event) => setState(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.zip_code}
+                value={zip_code}
+                onChange={(event) => setZipCode(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.phone}
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+                />
+                <input type="text"
+                placeholder={client.email}
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                />
+                <button className="a2o-btn" onClick={updateClientInfo}>Submit</button>
+            </div>
+            
+            {/* specific client's appointment history list */}
             <div className="card-half-right">
                 <h3>Appointment History</h3>
                 <ul>
@@ -81,14 +144,16 @@ function Profile(props){ //props here to get specific client id? params? redux?
                 <i class="fab fa-pagelines"></i>
                 <li className="li_asLink" onClick={apptDetails}>12/12/20 Winter Wellness</li>
                 </ul>
-                
-                {/* map through appt reducer to return all appts specific to this client */}
-                {appt.map((item, i) => {
-                    return(
-                    <ul key={i}>
-                        {/* <li onClick={() => apptDetails(item.id)}>{item.date}{item.appt_name}</li> */}
-                    </ul>)
-                })}
+
+                {/* code similar to Mary's lecture */}
+                {/* <ul>
+                    {appt.map((item, i) => 
+                        <li key={i} onClick={() => apptDetails(item.id)}>
+                            {item.date}{item.appt_name}
+                            <button>View Details(can also make li a link)</button>
+                        </li>
+                    )}
+                </ul> */}
                 {/* Appointment details also conditionally render depending on date */}
             </div>
         
@@ -97,3 +162,11 @@ function Profile(props){ //props here to get specific client id? params? redux?
 }
 
 export default Profile;
+
+//  {/* map through appt reducer to return all appts specific to this client */}
+//  {appt.map((item, i) => {
+//     return(
+//     <ul key={i}>
+//         {/* <li onClick={() => apptDetails(item.id)}>{item.date}{item.appt_name}</li> */}
+//     </ul>)
+// })}
